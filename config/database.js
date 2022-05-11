bb9dfae5af8c3afc36362d32fc2c04ed428b87c0
waly1979 <martinreg2014@gmail.com>
2022-05-10 23:02:25 -0300

    confiracion del database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.HOST,
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": "root",
