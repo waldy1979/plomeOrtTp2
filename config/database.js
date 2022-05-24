3a68e68f4f382b550d0b5c8a1ac9ff391ba86ffc
Pablo Litvin <pablo.litvin@gmail.com>
2022-05-24 19:58:05 -0300

    update admin

---
+++ config/database.js
@@ -0,0 +1,29 @@
+
+const dotenv = require('dotenv')
+
+dotenv.config(); 
+
+module.exports = {  
+    "development": {
+		"username": process.env.DB_USER,
+		"password": process.env.DB_PASS,
+		"database": process.env.DB_NAME,
+		"host": process.env.HOST,
+		"dialect": process.env.DB_DIALECT,
+    },
+    "test": {
+      "username": process.env.DB_USER,
+		  "password": process.env.DB_PASS,
+		  "database": process.env.DB_NAME,
+		  "host": process.env.HOST,
+		  "dialect": process.env.DB_DIALECT,
+    },
+    "production": {
+      "username": process.env.DB_USER,
+		  "password": process.env.DB_PASS,
+		  "database": process.env.DB_NAME,
+		  "host": process.env.HOST,
+		  "dialect": process.env.DB_DIALECT,
+    }
+  
+}
