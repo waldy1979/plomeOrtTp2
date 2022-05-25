b1ba76a9907daf31e8ff5b59c0ec068e3f15f1be
Rafael Dávila Pico <59027415+RADPi@users.noreply.github.com>
2022-05-25 10:49:33 -0300

    todas los rutas en el index

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const app = require('./src/server')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.set('port', process.env.PORT || 2999)
+
+app.use(cors())
+app.use(express.json({ extended: true }))
+
+app.get('/', (req, res) => {
+	res.send('Hola Mundo!!!')
+})
+
+app.use('/buildings', require('./routes/buildings.routes'))
 
 app.listen(app.get('port'), () => {
 	console.log(`Server on port ${app.get('port')}!`)
