e906b0967df8738aaf12235c09fe76285ed192b9
Rafael Dávila Pico <radp71@gmail.com>
2022-05-31 21:32:49 -0300

    addAdministrationIdOnBuildings repaired

--- src/db/migrations/20220531162600-addAdministrationIdOnBuildings.js
+++ src/db/migrations/20220531162600-addAdministrationIdOnBuildings.js
@@ -1,15 +1,13 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-
-     await queryInterface.addColumn('buildings', 'administrationId', {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.addColumn('buildings', 'administrationId', {
 			type: Sequelize.DataTypes.INTEGER,
 			allowNull: false,
-		}),
-  },
-
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('buildings', 'administrationId')
-  }
-};
+		})
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.removeColumn('buildings', 'administrationId')
+	},
+}
