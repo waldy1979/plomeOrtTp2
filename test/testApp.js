27a0443b1616e365776ab7410822a0b55d29daab
Rafael Dávila Pico <radp71@gmail.com>
2022-06-07 20:27:45 -0300

    testApp commented

--- test/testApp.js
+++ test/testApp.js
@@ -1,27 +1,26 @@
-const {assert}=require('chai');
+// const {assert}=require('chai');
 
+// describe('aprobarPresupuesto', function () {
+//     beforeEach(function () {
+//         let trabajo = new Trabajo;
+//         let aprobarPresupuesto = new aprobarPresupuesto;
+//     });
 
-describe('aprobarPresupuesto', function () {
-    beforeEach(function () {
-        let trabajo = new Trabajo;
-        let aprobarPresupuesto = new aprobarPresupuesto;
-    });
+//     // Si pasaron más de 5 días de la emisión del presupuesto, no puedo intentar marcar como aprobado
+//     it ('Que el presupuesto tiene menos de 5 días de emitido', function () {
+//         // crear la clase Trabajo
+//         // crear la clase Presupuesto
+//         // que tenga fecha de presupuesto emitido
+//         // que el estado del trabajo sea pendiente de aprobación.
+//         let estadoPresupuestoViejo = trabajo.getPresupuesto().getEstado();
+//         presupuesto.setFechaEmision('2022-04-19'); // tiene menos de 5 dias de emitido
+//         assert.doesNotThrow(() => aprobarPresupuesto.aprobar(trabajo))
+//         assert.notEqual(estadoPresupuestoViejo, trabajo.getPresupuesto().getEstado())
+//     });
 
-    // Si pasaron más de 5 días de la emisión del presupuesto, no puedo intentar marcar como aprobado
-    it ('Que el presupuesto tiene menos de 5 días de emitido', function () {
-        // crear la clase Trabajo
-        // crear la clase Presupuesto
-        // que tenga fecha de presupuesto emitido
-        // que el estado del trabajo sea pendiente de aprobación.
-        let estadoPresupuestoViejo = trabajo.getPresupuesto().getEstado();
-        presupuesto.setFechaEmision('2022-04-19'); // tiene menos de 5 dias de emitido
-        assert.doesNotThrow(() => aprobarPresupuesto.aprobar(trabajo))
-        assert.notEqual(estadoPresupuestoViejo, trabajo.getPresupuesto().getEstado())
-    });
+//     it ('Si no pasaron 4 meses lanza una excepcion', function () {
+//     });
 
-    it ('Si no pasaron 4 meses lanza una excepcion', function () {
-    });
-
-    it ('Lanza un excepcion si ya se recategorizo en este periodo', function () {
-    });
-});
+//     it ('Lanza un excepcion si ya se recategorizo en este periodo', function () {
+//     });
+// });
--- test/testBuildings.js
+++ test/testBuildings.js
@@ -1,28 +1,170 @@
 const axios = require('axios')
 const { assert } = require('chai')
 
-describe('Building ', function () {
-	beforeEach(async () => {
+describe('Building Create', () => {
+	/*
+  Debe incluir los siguientes campos: 
+  - address: String 50 caracteres, requerido
+  - city: String 40 caracteres, requerido
+  - manager: String 40 caracteres,  requerido
+  - cellPhone: String 40 caracteres, requerido
+  Al agregar un domicilio se debe verificar que no exista previamente, siendo los campos identificatorios la combinación del address + city.
+  Al agregar el domicilio se debe elegir la administración que usa, verificando su existencia previa.
+  */
+
+	it('Si se completan los datos mínimos por campo debe permitir la carga ', async () => {
+		const building = {
+			address: 'a',
+			city: 'a',
+			manager: 'a',
+			cellPhone: 'a',
+			AdministrationId: 1,
+		}
+		try {
+			const { status, data } = await axios.post(
+				'http://localhost:2999/buildings',
+				building,
+			)
+			assert.equal(status, 201)
+			await axios.delete(`http://localhost:2999/buildings/${data.building.id}`)
+		} catch (error) {}
+	})
+
+	it('Si se completan los datos máximos por campo debe permitir la carga ', async () => {
+		const building = {
+			address: 'abcde12345fghij67890abcde12345fghij67890abcde12345',
+			city: 'abcde12345fghij67890abcde12345fghij67890',
+			manager: 'abcde12345fghij67890abcde12345fghij67890',
+			cellPhone: 'abcde12345fghij67890abcde12345fghij67890',
+			AdministrationId: 1,
+		}
+		try {
+			const { status, data } = await axios.post(
+				'http://localhost:2999/buildings',
+				building,
+			)
+			assert.equal(status, 201)
+			await axios.delete(`http://localhost:2999/buildings/${data.building.id}`)
+		} catch (error) {}
+	})
+
+	it('Si falta address, no debe permitir la carga ', async () => {
+		const building = {
+			address: null,
+			city: 'Test',
+			manager: 'Test',
+			cellPhone: 'Test',
+			AdministrationId: 1,
+		}
+		try {
+			await axios.post('http://localhost:2999/buildings', building)
+		} catch (error) {
+			assert.equal(error.response.status, 422)
+		}
+	})
+
+	it('Si el address tiene mas de 50 caracteres no debe permitir la carga ', async () => {
+		const building = {
+			address: 'abcde12345fghij67890abcde12345fghij67890abcde12345+',
+			city: 'Test',
+			manager: 'Test',
+			cellPhone: 'Test',
+			AdministrationId: 1,
+		}
+		try {
+			await axios.post('http://localhost:2999/buildings', building)
+		} catch (error) {
+			assert.equal(error.response.status, 422)
+		}
+	})
+
+	it('Si falta city, no debe permitir la carga ', async () => {
+		const building = {
+			address: 'Test',
+			city: null,
+			manager: 'Test',
+			cellPhone: 'Test',
+			AdministrationId: 1,
+		}
+		try {
+			await axios.post('http://localhost:2999/buildings', building)
+		} catch (error) {
+			assert.equal(error.response.status, 422)
+		}
+	})
+
+	it('Si el city tiene mas de 40 caracteres no debe permitir la carga ', async () => {
+		const building = {
+			address: 'Test',
+			city: 'abcde12345fghij67890abcde12345fghij67890+',
+			manager: 'Test',
+			cellPhone: 'Test',
+			AdministrationId: 1,
+		}
+		try {
+			await axios.post('http://localhost:2999/buildings', building)
+		} catch (error) {
+			assert.equal(error.response.status, 422)
+		}
+	})
+
+	it('Si falta manager, no debe permitir la carga ', async () => {
+		const building = {
+			address: 'Test',
+			city: 'Test',
+			manager: null,
+			cellPhone: 'Test',
+			AdministrationId: 1,
+		}
+		try {
+			await axios.post('http://localhost:2999/buildings', building)
+		} catch (error) {
+			assert.equal(error.response.status, 422)
+		}
+	})
+
+	it('Si el manager tiene mas de 40 caracteres no debe permitir la carga ', async () => {
+		const building = {
+			address: 'Test',
+			city: 'Test',
+			manager: 'abcde12345fghij67890abcde12345fghij67890',
+			cellPhone: 'Test',
+			AdministrationId: 1,
+		}
+		try {
+			await axios.post('http://localhost:2999/buildings', building)
+		} catch (error) {
+			assert.equal(error.response.status, 422)
+		}
+	})
+
+	it('Si falta cellPhone, no debe permitir la carga ', async () => {
 		const building = {
 			address: 'Test',
 			city: 'Test',
 			manager: 'Test',
-			cellPhone: 'Test',
+			cellPhone: null,
+			AdministrationId: 1,
 		}
-		const resp = await axios.post(`buildings/`, building)
-		console.log(resp)
+		try {
+			await axios.post('http://localhost:2999/buildings', building)
+		} catch (error) {
+			assert.equal(error.response.status, 422)
+		}
 	})
 
-	it('Que no permita cargar un edificio duplicado', function () {
-		// let estadoPresupuestoViejo = trabajo.getPresupuesto().getEstado()
-		// presupuesto.setFechaEmision('2022-04-19') // tiene menos de 5 dias de emitido
-		// assert.doesNotThrow(() => aprobarPresupuesto.aprobar(trabajo))
-		// assert.notEqual(
-		// 	estadoPresupuestoViejo,
-		// 	trabajo.getPresupuesto().getEstado(),
+	it('Si el cellPhone tiene mas de 40 caracteres no debe permitir la carga ', async () => {
+		const building = {
+			address: 'Test',
+			city: 'Test',
+			manager: 'Test',
+			cellPhone: 'abcde12345fghij67890abcde12345fghij67890',
+			AdministrationId: 1,
+		}
+		try {
+			await axios.post('http://localhost:2999/buildings', building)
+		} catch (error) {
+			assert.equal(error.response.status, 422)
+		}
 	})
-
-	it('Si no pasaron 4 meses lanza una excepcion', function () {})
-
-	it('Lanza un excepcion si ya se recategorizo en este periodo', function () {})
 })
